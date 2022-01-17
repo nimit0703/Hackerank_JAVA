a7eb6a4c28f86c661b2f04ab333458b9347520ef
Nimit Trevadiya <68644815+nimit0703@users.noreply.github.com>
2022-01-17 20:57:19 +0530

    Create java-end-of-file.java

---
+++ java-end-of-file.java
@@ -0,0 +1,17 @@
+//https://www.hackerrank.com/challenges/java-end-of-file/problem?isFullScreen=true
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+     Scanner scan = new Scanner(System.in);
+   for(int i = 1; scan.hasNext()== true; i++){
+       System.out.println(i + " " + scan.nextLine());
+   }   
+    }
+   
+}
